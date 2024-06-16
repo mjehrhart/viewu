bae2517ff1e18b218bfef6410abbceb529adb077
Matthew Ehrhart <mjeoffline@gmail.com>
2024-06-16 09:15:08 -0700

    changed a few vlc options

--- NVR Viewer/ViewModel/RTSPPlayer2.swift
+++ NVR Viewer/ViewModel/RTSPPlayer2.swift
@@ -33,25 +33,42 @@ class PlayerUIView2: UIView, VLCMediaPlayerDelegate, ObservableObject{
         let url = URL(string: urlString)!
         let media = VLCMedia(url: url)
         
-        media.addOption("-vvvv")
-        media.addOptions(
-            [
-                //"--rtsp-tcp": true,
-                "--codec":"avcodec",
-                "--avcodec-hw":"none",
-                "--avcodec-fast" : true,
-                "--avcodec-threads" : "1",
-                "network-caching": 500, //300
-                "--rtsp-frame-buffer-size":500, //100
-                "--vout": "ios",
-                "--glconv" : "glconv_cvpx",
-                "--rtsp-caching=": 150,
-                "--tcp-caching=": 150,
-                "--realrtsp-caching=": 150, //150
-                //"--h264-fps": 20.0, //20.0
-                "--mms-timeout": 60000,
-            ]
-        )
+        //https://wiki.videolan.org/VLC_command-line_help
+        
+        media.addOption("--vv")
+        media.addOption("--codec=avcodec")
+        media.addOption("--avcodec-hw=any")
+        media.addOption("--avcodec-fast=true")
+        media.addOption("--avcodec-threads=0")
+        media.addOption("--network-caching=300")
+        media.addOption("--rtsp-frame-buffer-size=200")
+        media.addOption("--vout=ios")
+        media.addOption("--glconv=glconv_cvpx")
+        media.addOption("--rtsp-caching=150")
+        media.addOption("--tcp-caching=150")
+        media.addOption("--realrtsp-caching=150")
+        media.addOption("--mms-timeout=6000")
+        media.addOption("--h264-fps=15.0")
+        
+        
+//        media.addOptions(
+//            [
+//                //"--rtsp-tcp": true,
+//                "codec":"avcodec",
+//                "avcodec-hw":"any", // none
+//                "avcodec-fast" : true,
+//                "avcodec-threads" : 0, // "1" 0=auto
+//                "network-caching": 300, //300, 500
+//                "rtsp-frame-buffer-size":100, //100, 500
+//                "vout": "ios",
+//                "glconv" : "glconv_cvpx",
+//                "rtsp-caching": 150, //rtsp-caching=
+//                "tcp-caching": 150, //tcp-caching=
+//                "realrtsp-caching": 150, //150, realrtsp-caching=
+//                //"--h264-fps": 20.0, //20.0
+//                "mms-timeout": 6000, //60000
+//            ]
+//        )
         
         mediaPlayer.media = media
         mediaPlayer.delegate = self
